ec631ff6a13e478610c43beeda9745852f091493
JavierDiazDaw <javier.diaz.roman.al@iespoligonosur.org>
2022-01-12 00:28:21 +0100

    ejercicios 1 y 3

--- tema 6/ejercicio1/ejercicio1.js
+++ tema 6/ejercicio1/ejercicio1.js
@@ -1,37 +1,53 @@
-window.addEventListener("load", inicio);
+    window.addEventListener("load", inicio);
 
-function inicio(){
+    function inicio(){
 
-    var titulo = document.createElement('h1');
-    var txt1 = document.createTextNode('titulo');
-    titulo.appendChild(txt1);
+    //titulo
+    var titulo = document.createElement('title');
+    var t1 = document.createTextNode('texto titulo');
+    titulo.appendChild(t1);
 
+    //h1
+    var h1 = document.createElement('h1');
+    var txt1 = document.createTextNode('texto h1');
+    h1.appendChild(txt1);
+
+    //parrafo
     var parrafo = document.createElement('p');
     
     var txtp1 = document.createTextNode('texto1');
-    var a = document.createElement('a');
+    var enlace1 = document.createElement('a');
+
     var txta1 = document.createTextNode('textoEnlace1');
     parrafo.appendChild(txtp1);
-    parrafo.appendChild(a);
-    a.appendChild(txta1)
+    parrafo.appendChild(enlace1);
+    enlace1.appendChild(txta1)
+
+    enlace1.setAttribute("href", "https://www.google.es")
 
     var txtp2 = document.createTextNode('texto2');
-    var a = document.createElement('a');
+    var enlace2 = document.createElement('a');
     var txta2 = document.createTextNode('textoEnlace2');
     parrafo.appendChild(txtp2);
-    parrafo.appendChild(a);
-    a.appendChild(txta2)
+    parrafo.appendChild(enlace2);
+    enlace2.appendChild(txta2)
+
+    enlace2.setAttribute("href", "https://www.google.es")
 
     var txtp3 = document.createTextNode('texto3');
-    var a = document.createElement('a');
+    var enlace3 = document.createElement('a');
     var txta3 = document.createTextNode('textoEnlace3');
     parrafo.appendChild(txtp3);
-    parrafo.appendChild(a);
-    a.appendChild(txta3)
+    parrafo.appendChild(enlace3);
+    enlace3.appendChild(txta3)
+
+    enlace3.setAttribute("href", "https://www.google.es")
 
     var txtp4 = document.createTextNode('texto4');
     parrafo.appendChild(txtp4);
 
-    document.body.appendChild(titulo);
+    document.head.appendChild(titulo);
+    document.body.appendChild(h1);
     document.body.appendChild(parrafo);
+
 }
---
+++ tema 6/ejercicio3/ejercicio3.js
@@ -0,0 +1,20 @@
+window.addEventListener("load", inicio);
+
+function inicio(){
+
+    //ul
+    var ul = document.createElement('ul');
+
+    //li    
+    var li1 = document.getElementsByTagName("li").type="square";
+    var coche1 = document.createTextNode("Ford");
+    li1.appendChild(coche1);
+    
+    
+    var li2 = document.getElementsByTagName("li").type="square";
+    var coche2 = document.createTextNode("BMV");
+    //li2.appendChild(coche2);
+
+    //ul.appendChild(li1);
+    //ul.appendChild(li2);
+}
